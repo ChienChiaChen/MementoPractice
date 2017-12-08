8a908e915d7b469e5bbea3796e3d5fc909dcfc2d
JasonChien <s124202468@gmail.com>
2017-12-09 06:41:42 +0800

    Create architecture

--- app/src/main/java/com/chiachen/mementopractice/MainActivity.java
+++ app/src/main/java/com/chiachen/mementopractice/MainActivity.java
@@ -1,13 +1,86 @@
 package com.chiachen.mementopractice;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int MAX_RECORD = 30;
+    private EditText mEditText;
+    private TextView mRedo;
+    private TextView mUndo;
+    private TextView mSave;
+
+    List<Memento> mMementos = new ArrayList<>(MAX_RECORD);
+    int mIndex = 0;
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.redo_btn: {
+                    restoreText(getNextMemento());
+                    makeToast("restore");
+                    break;
+                }
+                case R.id.undo_btn: {
+                    restoreText(getPrevMemento());
+                    makeToast("undo");
+                    break;
+                }
+                case R.id.save_btn: {
+                    saveMemento(createMementoFromEditText());
+                    makeToast("save");
+                    break;
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mEditText = (EditText) findViewById(R.id.note_edittext);
+        mRedo = (TextView) findViewById(R.id.redo_btn);
+        mSave = (TextView) findViewById(R.id.save_btn);
+        mUndo = (TextView) findViewById(R.id.undo_btn);
+
+        mRedo.setOnClickListener(mOnClickListener);
+        mSave.setOnClickListener(mOnClickListener);
+        mUndo.setOnClickListener(mOnClickListener);
+    }
+
+    public void makeToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void restoreText(Memento memento) {
+    }
+
+    public Memento getPrevMemento() {
+        return null;
+    }
+
+    public Memento getNextMemento() {
+        return null;
+    }
+
+    private Memento createMementoFromEditText() {
+        return null;
+    }
+
+    private void saveMemento(Memento memento) {
     }
 }
---
+++ app/src/main/java/com/chiachen/mementopractice/Memento.java
@@ -0,0 +1,10 @@
+package com.chiachen.mementopractice;
+
+/**
+ * Created by jianjiacheng on 09/12/2017.
+ */
+
+public class Memento {
+    public String text;
+    public int cursor;
+}
